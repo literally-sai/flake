078365c5f72f52f65e440dd6562192e9ce2879b6
literally-sai <sai@literally-sai.com>
2026-01-25 13:37:22 +0100

    Add marathon theme

---
+++ home/rice/themes/marathon.nix
@@ -0,0 +1,71 @@
+{
+  kitty = {
+    background = "#0c0c1c";
+    background_opacity = "0.9";
+    foreground = "#a7a8af";
+    cursor = "#c0d8f0";
+    selection_background = "#181830";
+    color0 = "#0c0c1c";
+    color8 = "#c51459";
+    color1 = "#ff293b";
+    color9 = "#ff6600";
+    color2 = "#01b77e";
+    color10 = "#141623";
+    color3 = "#c3ff0b";
+    color11 = "#181830";
+    color4 = "#7e88d3";
+    color12 = "#6d6f7c";
+    color5 = "#fb74b8";
+    color13 = "#b2ccd6";
+    color6 = "#9499cb";
+    color14 = "#01ffb0";
+    color7 = "#a7a8af";
+    color15 = "#c0d8f0";
+    selection_foreground = "#a7a8af";
+    active_tab_foreground = "#a7a8af";
+    active_tab_background = "#7e88d3";
+    inactive_tab_foreground = "#a7a8af";
+    inactive_tab_background = "#181830";
+  };
+
+  waybar = {
+    background = "rgba(12, 12, 28, 0.9)";
+    foreground = "#A7A8AF";
+    highlight = "#7E88D3";
+  };
+
+  borders = {
+    active0 = "7E88D3FF";
+    active1 = "FF293BFF";
+    inactive0 = "181830FF";
+    inactive1 = "141623FF";
+  };
+
+  rofi = {
+    background0 = "rgba(12, 12, 28, 0.6)";
+    background1 = "#14162380";
+    foreground = "#A7A8AF";
+    selected = "#C0D8F0";
+    active = "#01B77E";
+    urgent = "#FF293B";
+  };
+
+  images = [
+    {
+      name = "rofi.png";
+      source = ../../../resources/marathon/rofi.png;
+    }
+    {
+      name = "pfp.png";
+      source = ../../../resources/marathon/pfp.png;
+    }
+    {
+      name = "lockpaper.png";
+      source = ../../../resources/marathon/lockpaper.png;
+    }
+    {
+      name = "wallpaper.png";
+      source = ../../../resources/marathon/wallpaper.png;
+    }
+  ];
+}
