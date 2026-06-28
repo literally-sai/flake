5f15b24507271341eb39953c52889373f4f78d3b
literally-sai <sai@literally-sai.com>
2026-06-28 22:06:19 +0200

    add bevy_cli

--- shells/bevy/flake.nix
+++ shells/bevy/flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    bevy_cli.url = "github:TheBevyFlock/bevy_cli";
   };
 
   outputs =
@@ -12,6 +13,7 @@
       nixpkgs,
       rust-overlay,
       flake-utils,
+      bevy_cli,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -26,25 +28,25 @@
         devShells.default =
           with pkgs;
           mkShell {
-            buildInputs =
-              [
-                (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
-                pkg-config
-                clang
-                mold
-              ]
-              ++ lib.optionals (lib.strings.hasInfix "linux" system) [
-                alsa-lib
-                vulkan-loader
-                vulkan-tools
-                libudev-zero
-                libx11
-                libxcursor
-                libxi
-                libxrandr
-                libxkbcommon
-                wayland
-              ];
+            buildInputs = [
+              (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
+              pkg-config
+              clang
+              mold
+              bevy_cli.packages.${system}.default
+            ]
+            ++ lib.optionals (lib.strings.hasInfix "linux" system) [
+              alsa-lib
+              vulkan-loader
+              vulkan-tools
+              libudev-zero
+              libx11
+              libxcursor
+              libxi
+              libxrandr
+              libxkbcommon
+              wayland
+            ];
             RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
             LD_LIBRARY_PATH = lib.makeLibraryPath [
               vulkan-loader
